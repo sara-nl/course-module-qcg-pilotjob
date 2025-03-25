0beb542b43e269281a15b46e90d4063c4e106e29
Maxim <maxim.vadimovich@gmail.com>
2025-03-25 11:16:02 +0100

    Update qcg_job.sh

--- hands-on/example_03/qcg_job.sh
+++ hands-on/example_03/qcg_job.sh
@@ -4,8 +4,8 @@
 #SBATCH --time=00:05:00
 #SBATCH --partition=rome
 
-module load 2023
-module load QCG-PilotJob/0.14.1-gfbf-2023a
+module load 2024
+module load QCG-PilotJob/0.14.1-gfbf-2024a
 
 python3 qcg_job.py
 
